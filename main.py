008e6d2142238d291b423611e8e601c32becbb0a
Francesco Saponara <mobile@weblink.it>
2025-04-10 15:32:14 +0200

    added .env file with API_KEY

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
+import os
 
 import requests
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
                              QLineEdit, QPushButton, QVBoxLayout)
 
+from dotenv import load_dotenv, dotenv_values
+
 from PyQt5.QtCore import Qt
+
+load_dotenv()
+
+print(os.getenv("WEATHER_APIKEY"))
 
 class WeatherApp(QWidget):
     def __init__(self):
